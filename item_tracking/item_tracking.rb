6831ff70fc062e4d769540ab592171a89b74cba2
Robert Koch <rkoch.mmt-m2013@fh-salzburg.ac.at>
2014-05-24 15:57:10 +0200

    basic item-tracker-structure

--- item_tracking/item_tracking.rb
+++ item_tracking/item_tracking.rb
@@ -1,21 +1,54 @@
 #!/usr/bin/ruby
 require 'grape'
+require 'json'
 
 module ItemTracking
+	
+	class ItemManager
+		def initialize
+			@items = []
+			@next_id = 1
+		end
+
+		def all
+			@items
+		end
+
+		def find(item_id)
+		end
+
+		def create(name, location_id)
+		end
+
+		def delete(item_id)
+		end
+	end
+
 	class API < Grape::API
 		format :json
+
+		item_manager = ItemManager.new
 		
+		desc "Show all items"
 	  get :items do
-	    {hello: "world"}
+	    item_manager.all
 	  end
 
+	  desc "create new item"
+		params do
+      requires :name, type: String, desc: "item name"
+      requires :location, type: Integer, desc: "item's location id"
+    end
 	  post :items do
-	    {hello: "world"}
+	    #not implemented yet
 	  end
 
-	  #params: id
+	  desc "delete item"
+	  params do
+	  	requires :id, type: Integer, desc: "item id"
+	  end
 	  delete :item do
-	    {hello: "world"}
+	    #not implemented yet
 	  end
 	end
 end
