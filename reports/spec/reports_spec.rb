c3b504169fd708774b928c2a985aee93af170aea
Robert Koch <rkoch.mmt-m2013@fh-salzburg.ac.at>
2014-05-28 16:44:59 +0200

    change report test setup

--- reports/spec/reports_spec.rb
+++ reports/spec/reports_spec.rb
@@ -1,11 +1,8 @@
 #!/usr/bin/ruby
-Bundler.require
-
-require 'rack/test'
+require 'spec_helper'
 require_relative '../lib/reports'
 
 describe Reports::API do
-  include Rack::Test::Methods
 
   def app
     Reports::API
