6dcb3551b57b96423bd877dac8d86f9619b2e05c
Robert Koch <rkoch.mmt-m2013@fh-salzburg.ac.at>
2014-05-30 00:12:16 +0200

    report-app tests

--- reports/spec/reports_spec.rb
+++ reports/spec/reports_spec.rb
@@ -2,6 +2,8 @@
 require 'spec_helper'
 require_relative '../lib/reports'
 
+final_result = @final_result
+
 describe Reports::API do
 
   def app
@@ -13,10 +15,35 @@ describe Reports::API do
   	describe "authorized" do
   		before do
   			authorize 'paul', 'thepanther'
+
+        stub_request(:get, "http://paul:thepanther@localhost:9191/user").
+          to_return(:status => 200, :body => "", :headers => {})
   		end
+
+      describe "GET /reports/by-location" do
+        it "returns status 200 and @final_report in json-format" do
+          get "/reports/by-location"
+          last_response.status.should == 200
+          last_response.body.should == JSON.dump(final_result)
+        end
+      end
   	end
 
 		describe "unauthorized" do
+      before do
+        authorize 'false', 'user'
+
+        stub_request(:get, "http://false:user@localhost:9191/user").
+          to_return(:status => 403, :body => "", :headers => {})
+      end
+
+      describe "GET /reports/by-location" do
+        it "returns status 403 and error message" do
+          get "/reports/by-location"
+          last_response.status.should == 403
+          last_response.body.should == '{"error":"NO report"}'
+        end
+      end
   	end
 
   end
--- reports/spec/spec_helper.rb
+++ reports/spec/spec_helper.rb
@@ -9,3 +9,40 @@ end
 
 include WebMock::API
 WebMock.disable_net_connect!(allow_localhost: false)
+
+items = [
+	{
+		name: 'item1',
+		location: 1,
+		id: 1
+	},
+	{
+		name: 'item2',
+		location: 2,
+		id: 2
+	}
+]
+
+locations = [
+	{
+		name: 'location1',
+		address: 'address1',
+		id: 1
+	},
+	{
+		name: 'location2',
+		address: 'address2',
+		id: 2
+	}
+]
+
+@final_result = [
+	locations[0].merge(items: [items[0]]),
+	locations[1].merge(items: [items[1]])
+]
+
+stub_request(:get, "http://paul:thepanther@localhost:9292/items").
+  to_return(:status => 200, :body => JSON.dump(items), :headers => {})
+
+stub_request(:get, "http://paul:thepanther@localhost:9393/locations").
+  to_return(:status => 200, :body => JSON.dump(locations), :headers => {})
